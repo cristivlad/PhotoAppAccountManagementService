4f47bd5d2874f7be7b44ab0fcb72fbef38344746
cristiii <cristian.vlad10@gmail.com>
2023-01-29 20:27:49 +0200

    init

---
+++ src/main/java/com/example/photoappaccountmanagementservice/PhotoAppAccountManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.photoappaccountmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppAccountManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppAccountManagementServiceApplication.class, args);
+    }
+
+}
